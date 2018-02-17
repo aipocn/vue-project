e6b361bdd14d1bb57c5aef1ff390de8168fefc0d
周刚 <zg@GangMBP.lan>
2018-02-17 22:12:36 +0800

    修改代码格式

--- src/config/menu.js
+++ src/config/menu.js
@@ -1,34 +1,34 @@
 module.exports = [{
-        name: '资产概览',
-        id: '1',
-        icon: 'el-icon-menu',
-        path:'/',
-        component: 'index'
-    },{
-        name: '我的资产',
-        id: '2',
-        icon: 'el-icon-menu',
-        path:'/AssetManage',
-        component: 'AssetManage/index'
-    },{
-        name: '部门资产',
-        id: '3',
-        icon: 'el-icon-menu',
-        path:'/AssetClass',
-        component: 'AssetManage/AssetClass'
-    },{
-        name: '资产管理',
-        id: '4',
-        icon: 'el-icon-menu',
-        sub: [{
-            name: '资产录入',
-            id: '4-1',
-            path:'',
-            component: '4-1'
-        },{
-            name: '资产分类',
-            id: '4-2',
-            path:'',
-            component: 'AssetManage/AssetClass'
-        }]
+  name: '资产概览',
+  id: '1',
+  icon: 'el-icon-menu',
+  path: '/',
+  component: 'index'
+}, {
+  name: '我的资产',
+  id: '2',
+  icon: 'el-icon-menu',
+  path: '/AssetManage',
+  component: 'AssetManage/index'
+}, {
+  name: '部门资产',
+  id: '3',
+  icon: 'el-icon-menu',
+  path: '/AssetClass',
+  component: 'AssetManage/AssetClass'
+}, {
+  name: '资产管理',
+  id: '4',
+  icon: 'el-icon-menu',
+  sub: [{
+    name: '资产录入',
+    id: '4-1',
+    path: '',
+    component: '4-1'
+  }, {
+    name: '资产分类',
+    id: '4-2',
+    path: '',
+    component: 'AssetManage/AssetClass'
+  }]
 }]
